9b292329bb6a5fc1e4278de0886b2e9897e6693d
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 15:54:31 -0700

    destructur monster obj in CardList.jsx

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -7,14 +7,17 @@ class CardList extends Component {
 
     return (
       <div className="card-list">
-        {monsters.map(monster => (
-          <div className="card-container" key={monster.id}>
+        {monsters.map(monster => {
+          const { name, email, id } = monster;
+          return (
+          <div className="card-container" key={id}>
             <img 
-            alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
-            <h2>{monster.name}</h2>
-            <p>monster.email</p>
+            alt={`monster ${name}`}
+            src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
           </div>
-        ))}
+        )})}
       </div>
     )
   }
