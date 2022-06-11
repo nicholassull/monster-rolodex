d35e46aec96a808e2fe6eaf28263f7ae4b512e76
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 18:00:38 -0700

    add prop destructuring to Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,7 @@
 import './Card.styles.css';
 
-const Card = (props) => { 
-    const { name, email, id } = props.monster;
+const Card = ({ monster }) => { 
+    const { name, email, id } = monster;
     return (
       <div className="card-container" key={id}>
         <img 
