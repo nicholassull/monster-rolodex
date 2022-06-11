3fb0fdf8b425b49edd8369b9f087922114c6edea
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 17:59:38 -0700

    update Card to functional component

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -4,13 +4,13 @@ import Card from './../card/Card';
 import { render } from "@testing-library/react";
 
 //Destructuring can be done right in the fn argument.
-const CardList = ({ monsters }) => {
+const CardList = ({ monsters }) => (
   //Return() is implicit
   <div className="card-list">
     {monsters.map(monster => {
       return <Card monster={monster} />
     })}
   </div>
-}
+);
 
 export default CardList;
