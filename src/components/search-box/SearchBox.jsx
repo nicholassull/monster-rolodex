eaa10d76b1e28325ddeba304f472f9950b2419ff
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 18:02:21 -0700

    update SearchBox to functional component

--- src/components/search-box/SearchBox.jsx
+++ src/components/search-box/SearchBox.jsx
@@ -2,19 +2,13 @@ import { Component } from "react";
 import './SearchBox.styles.css';
 
 
-class SearchBox extends Component {
-
-  render() {
-    return (
-      // onChange handler runs every time the value in the search bar changes
-      <input 
-      type='search'
-      className={`search-box ${this.props.className}`}
-      placeholder={this.props.placeholder} 
-      onChange={this.props.onChangeHandler} 
-      />
-    )
-  }
+const SearchBox = ({className, placeholder, onChangeHandler}) => {
+  <input 
+  type='search'
+  className={`search-box ${className}`}
+  placeholder={placeholder} 
+  onChange={onChangeHandler} 
+  />
 }
 
 export default SearchBox;
