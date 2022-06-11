4c417d46729f6c261251c5563cb8cd7300bf410d
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 18:24:37 -0700

    upgrade app to React 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; //For React 17
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+// For React 18
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+  );
+
+// For React 17
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
